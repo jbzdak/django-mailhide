d5189c424f55e2428b7a39458e381cf71e71ad49
Jacek Bzdak <jbzdak@gmail.com>
2016-01-21 21:57:39 +0100

    Fix for first issue

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with codecs.open("readme.rst", encoding="utf-8") as f:
 
 setup(
     name='django-mailhide',
-    version='1.0.1',
+    version='1.0.2',
     packages=['django_mailhide', 'django_mailhide.templatetags'],
     url='https://github.com/jbzdak/django-mailhide',
     license='BSD-style two clause',
@@ -24,5 +24,5 @@ Natural Language :: Polish
 Natural Language :: English
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3.3
-    """
+    """.strip().split('\n')
 )
